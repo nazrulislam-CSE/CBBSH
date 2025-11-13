669bd9209195cce30b5555f2702a4a62ea9cfedf
nazrulislam-CSE <nsuzon02@gmail.com>
2025-11-13 10:30:45 +0600

    update frontend pages

--- resources/views/frontend/menu/index.blade.php
+++ resources/views/frontend/menu/index.blade.php
@@ -1,20 +1,31 @@
 @extends('layouts.frontend.app', ['pageTitle' => $page->page_title])
 
 @section('content')
-
 <!-- Breadcrumb Banner Start -->
-{{-- <section class="breadcrumb-banner" style="background-image: url('{{ asset('frontend/assets/img/breadcrumb-bg.jpg') }}'); padding: 100px 0; text-align: center; color: #fff;">
-    <div class="container">
-        <h1 class="page-title">{{ $page->page_title }}</h1>
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb justify-content-center">
-                <li class="breadcrumb-item"><a href="{{ route('frontend.home') }}" style="color: #fff;">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">{{ $page->page_title }}</li>
-            </ol>
-        </nav>
-    </div>
-</section> --}}
+<section class="breadcrumb-banner" 
+    style="
+        background-image: url('{{ !empty($page->image) ? url('upload/page/' . $page->image) : url('frontend/img/slider/7.jpg') }}');
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+        padding: 200px 0;
+        text-align: center;
+        color: #fff;
+    ">
+    <!--<div class="container">-->
+    <!--    <h1 class="page-title">{{ $page->page_title }}</h1>-->
+    <!--    <nav aria-label="breadcrumb">-->
+    <!--        <ol class="breadcrumb justify-content-center">-->
+    <!--            <li class="breadcrumb-item">-->
+    <!--                <a href="{{ route('frontend.home') }}" style="color: #fff;">Home</a>-->
+    <!--            </li>-->
+    <!--            <li class="breadcrumb-item active" aria-current="page">{{ $page->page_title }}</li>-->
+    <!--        </ol>-->
+    <!--    </nav>-->
+    <!--</div>-->
+</section>
 <!-- Breadcrumb Banner End -->
+
 
 <!-- Page Content -->
 <section class="page-content pt-5 pb-5">
